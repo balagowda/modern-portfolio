60c59430a8a19345671a75bec59fa8ccf0c34634
balagowda <balagowda9483@gmail.com>
2025-12-13 23:26:05 +0530

    config change for github pages

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
+
+export default nextConfig;
