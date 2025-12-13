0c3779a74c6b8b72d9fb739f8a2133bf2b0d3593
balagowda <balagowda9483@gmail.com>
2025-12-13 23:46:59 +0530

    config change for github pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+
+  basePath: "/modern-portfolio",
+  assetPrefix: "/modern-portfolio/",
+
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
