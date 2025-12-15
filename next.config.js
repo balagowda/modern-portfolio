a467534c970eb51d3a764e3e2d3722a0e29fac5e
balagowda <balagowda9483@gmail.com>
2025-12-15 23:01:42 +0530

    Remove basePath and rebuild for custom domain

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-
-  basePath: "/modern-portfolio",
-  assetPrefix: "/modern-portfolio/",
-
   images: {
     unoptimized: true,
   },
 };
 
 module.exports = nextConfig;
+
