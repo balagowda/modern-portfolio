511750e417052619f40b476b88765a209716d8da
balagowda <balagowda9483@gmail.com>
2025-12-16 10:35:15 +0530

    visible on google

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,33 @@ import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Balachandregowda P",
-  description: "Porfolio website",
+export const metadata = {
+  title: "Balachandregowda | Java Developer & System Engineer",
+  description:
+    "Bytes by Bala – Portfolio of Balachandregowda, Java & Spring Boot developer, AWS certified, sharing projects, blogs, and tech insights.",
+  keywords: [
+    "Bytes by Bala",
+    "Balachandregowda",
+    "bytesbybala",
+    "Balachandra",
+    "Java Developer",
+    "Spring Boot",
+    "Microservices",
+    "System Engineer",
+    "AWS Certified",
+    "Programming",
+    "Software Development",
+    "Tech Projects",
+    "AWS",
+    "Portfolio",
+  ],
   icons: {
     icon: '/favicon.ico',
   },
+  authors: [{ name: "Balachandregowda" }],
+  metadataBase: new URL("https://www.bytesbybala.com"),
 };
+
 
 export default function RootLayout({
   children,
